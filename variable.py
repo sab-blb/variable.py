160bfcf07ee9756c203b0644f18eef94e476e0f4
Alexandr <alexandr_balabanov55@mail.ru>
2024-08-03 17:27:46 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+course = 'Python,'
+total_tasks = 12
+hours_spent = 1.5
+AverageExecutionTime = (hours_spent / total_tasks)
+print("Курс:", course, "всего задач:", total_tasks, ", затрачено часов", hours_spent, ", среднее время выполнения",
+      AverageExecutionTime, "часа")
